d7f98c457a953685a75d280c045a9612e3b49a89
fengo4142 <fengo4142@gmail.com>
2017-11-12 21:11:45 -0800

    passing a failing test and adding flow and other react scaffolding

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import  App from './App';
 
-const App = shallow(<App />);
+const app = shallow(<App />);
 
 it('renders correctly', () => {
     expect(app).toMatchSnapshot();
