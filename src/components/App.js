e93a1ba87c32900aa725a9143f31c2669216403f
fengo4142 <fengo4142@gmail.com>
2017-11-18 21:40:39 -0800

    refactoring tests for coverage, adding helper max_number

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Button } from "react-bootstrap";
 import Gift from "./Gift";
+import { max_number } from "../helper";
 
 class App extends Component {
   constructor() {
@@ -12,9 +13,9 @@ class App extends Component {
   addGift = () => {
     const { gifts } = this.state;
     const ids = this.state.gifts.map(gift => gift.id);
-    const max_id = ids.length > 0 ? Math.max(...ids) : 0;
+    const max_id = max_number(ids);
 
-    gifts.push({ id: max_id + 1 });
+    gifts.push({ id: max_number(ids) + 1 });
 
     this.setState({ gifts });
   };
